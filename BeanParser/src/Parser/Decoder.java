8c5d81aa7a36c40354cb099355b6d1c0d000a268
littlebeanfang <fangwenjing_sjtu.126.com>
2014-11-04 20:17:12 +0800

    Bug fix: avoid circle in parse. Jia changed.

--- BeanParser/src/Parser/Decoder.java
+++ BeanParser/src/Parser/Decoder.java
@@ -28,10 +28,15 @@ public class Decoder {
 		ParseAgenda pa=new ParseAgenda();
 		Object[] instret=new Object[2];
 		FeatureVector fvforinst=new FeatureVector();
+		
+		// set: disjoint-set data structure, stores the parent of each node
+		int[] set = new int[inst.length()];
+		for (int i = 0;i < inst.length();i++) set[i] = i;
+		
 		for(int orderindex=1;orderindex<inst.length();orderindex++){
 			//skip root node
 			int parseindex=ordermap.get(orderindex);
-			Object[] ret=this.FindHeadForOneWord(inst, parseindex, pa);
+			Object[] ret=this.FindHeadForOneWord(inst, parseindex, pa, set);
 			int parsehead=(int) ret[0];
 //			System.out.println("DecodeInstance fvforinst after call findhead:"+ret[1].toString().split(" ").length);
 			pa.AddArc(parseindex, parsehead);
@@ -44,6 +49,55 @@ public class Decoder {
 		instret[0]=pa;
 		instret[1]=fvforinst;
 		return instret;
+	}
+	
+	public Object[] FindHeadForOneWord(DependencyInstance inst,int childindex, ParseAgenda pa, int[] set){
+		Object[] ret=new Object[2];
+		boolean verbose=false;
+		int headindex=-1;
+		double score=Double.NEGATIVE_INFINITY;
+		FeatureVector actfv=new FeatureVector();
+		for(int head=0; head<inst.length();head++){
+			if ((head!=childindex) && (FindRoot(head, set) != childindex)) { //Jia: if the root of the head is not child
+				FeatureVector fv=new FeatureVector();
+				//pipe.AddNewFeature(inst, childindex, head, pa, fv);
+				pipe.extractFeatures(inst, childindex, head, pa, fv);
+				double temp=fv.getScore(param.parameters);
+				if(temp>score){
+					score=temp;
+					headindex=head;
+					//must store best fv in DependencyInstance
+					actfv=fv;
+					
+				}
+			}
+		}
+		if(verbose){
+			System.out.println("Instance:/n"+inst);
+			System.out.println("Child index:"+childindex);
+			System.out.println("Head index:"+headindex);
+			System.out.println("Score:"+score);
+		}
+		
+		//Update the disjoint-set
+		set[childindex] = headindex;
+		
+		//inst.fv.cat(actfv);
+		//Bean: store feature vector in fvforinst, for Object d[][]
+//		System.out.println("==================FindHeadForOneWord");
+//		System.out.println("actfv while decoding:"+actfv.toString().split(" ").length+"   fv:"+actfv.toString());
+		//System.out.println("fvforinst while decoding before cat:length"+fvforinst.toString().split(" ").length);
+		//fvforinst=fvforinst.cat(actfv);
+		//System.out.println("fvforinst while decoding:length"+fvforinst.toString().split(" ").length+"   fv:"+fvforinst.toString());
+//		System.out.println("==================FindHeadForOneWord END");
+		ret[0]=headindex;
+		ret[1]=actfv;
+		return ret;
+	}
+
+	private int FindRoot(int node, int[] set) {
+		if (set[node] != node) set[node] = FindRoot(set[node], set);
+		return set[node];
 	}
 	
 	public void PrintScores(DependencyInstance inst, ParseAgenda pa) throws IOException {
@@ -69,45 +123,5 @@ public class Decoder {
 			writer.write("\n");
 		}
 		writer.close();
-	}
-	
-	public Object[] FindHeadForOneWord(DependencyInstance inst,int childindex, ParseAgenda pa){
-		Object[] ret=new Object[2];
-		boolean verbose=false;
-		int headindex=-1;
-		double score=Double.NEGATIVE_INFINITY;
-		FeatureVector actfv=new FeatureVector();
-		for(int head=0; head<inst.length();head++){
-			if(head!=childindex){
-				FeatureVector fv=new FeatureVector();
-				//pipe.AddNewFeature(inst, childindex, head, pa, fv);
-				pipe.extractFeatures(inst, childindex, head, pa, fv);
-				double temp=fv.getScore(param.parameters);
-				if(temp>score){
-					score=temp;
-					headindex=head;
-					//must store best fv in DependencyInstance
-					actfv=fv;
-					
-				}
-			}
-		}
-		if(verbose){
-			System.out.println("Instance:/n"+inst);
-			System.out.println("Child index:"+childindex);
-			System.out.println("Head index:"+headindex);
-			System.out.println("Score:"+score);
-		}
-		//inst.fv.cat(actfv);
-		//Bean: store feature vector in fvforinst, for Object d[][]
-//		System.out.println("==================FindHeadForOneWord");
-//		System.out.println("actfv while decoding:"+actfv.toString().split(" ").length+"   fv:"+actfv.toString());
-		//System.out.println("fvforinst while decoding before cat:length"+fvforinst.toString().split(" ").length);
-		//fvforinst=fvforinst.cat(actfv);
-		//System.out.println("fvforinst while decoding:length"+fvforinst.toString().split(" ").length+"   fv:"+fvforinst.toString());
-//		System.out.println("==================FindHeadForOneWord END");
-		ret[0]=headindex;
-		ret[1]=actfv;
-		return ret;
 	}
 }
