c37d947f7eadc2b46fe7a5bfce3afb2e61c91d83
EastonWang <eastonwyz@gmail.com>
2014-12-27 15:59:35 +0800

    reformat

--- BeanParser/src/Test/ParseAgendaTest.java
+++ BeanParser/src/Test/ParseAgendaTest.java
@@ -1,5 +1,8 @@
 package Test;
 
+import DataStructure.DependencyInstance;
+import DataStructure.ParseAgenda;
+import IO.CONLLReader;
 import gnu.trove.TIntIntHashMap;
 import gnu.trove.TIntIntIterator;
 
@@ -9,25 +12,27 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import DataStructure.DependencyInstance;
-import DataStructure.ParseAgenda;
-import IO.CONLLReader;
-
 public class ParseAgendaTest {
-	public void ReadOneTreeTest(String onetreeconllfile) throws IOException{
-		CONLLReader reader = new CONLLReader();
+    public static void main(String args[]) throws IOException {
+        ParseAgendaTest pat = new ParseAgendaTest();
+        //pat.ReadOneTreeTest("wsj_2-21_malt_processindex_new.txt");
+        pat.ReadOneTreeTest("1sen.txt");
+    }
+
+    public void ReadOneTreeTest(String onetreeconllfile) throws IOException {
+        CONLLReader reader = new CONLLReader();
         reader.startReading(System.getenv("CODEDATA") + File.separator + onetreeconllfile);
         DependencyInstance instance = reader.getNext();
-        Map<String, Integer> structurecount=new HashMap<String, Integer>();
+        Map<String, Integer> structurecount = new HashMap<String, Integer>();
         while (instance != null) {
-        	ParseAgenda pa = new ParseAgenda();
-        	TIntIntHashMap ordermap = instance.orders;
-        	int[] heads = instance.heads;
+            ParseAgenda pa = new ParseAgenda();
+            TIntIntHashMap ordermap = instance.orders;
+            int[] heads = instance.heads;
             for (int orderindex = 1; orderindex < instance.length(); orderindex++) {
                 //skip root node
                 int parseindex = ordermap.get(orderindex);
                 int parsehead = heads[parseindex];
-                
+
                 pa.ChildProcess(parseindex, parsehead);
                 pa.AddArc(parseindex, parsehead);
             }
@@ -42,26 +47,23 @@ public class ParseAgendaTest {
 //            System.out.println("5 left child:"+pa.leftchilds.get(5));
         }
         PrintMap(structurecount);
-	}
-	public void PrintMap(Map<String, Integer> map){
-		Iterator iterator=map.entrySet().iterator();
-		while(iterator.hasNext()){
-			Map.Entry<String, Integer> entry=(Map.Entry<String, Integer>)iterator.next();
-			//System.out.println("Structure:"+entry.getKey()+",Count:"+entry.getValue());
-			//System.out.println("Structure:"+entry.getKey()+"\t"+entry.getValue());
-			System.out.println(entry.getKey()+"\t"+entry.getValue());
-		}
-	}
-	public void PrintTIntIntMap(TIntIntHashMap map,String prefix){
-		System.out.println(prefix);
-		 for ( TIntIntIterator it = map.iterator(); it.hasNext(); ) {
-			   it.advance();
-			   System.out.println("Head:"+it.key()+",count:"+it.value());
-		 }
-	}
-	public static void main(String args[]) throws IOException{
-		ParseAgendaTest pat=new ParseAgendaTest();
-		//pat.ReadOneTreeTest("wsj_2-21_malt_processindex_new.txt");
-		pat.ReadOneTreeTest("1sen.txt");
-	}
+    }
+
+    public void PrintMap(Map<String, Integer> map) {
+        Iterator iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>) iterator.next();
+            //System.out.println("Structure:"+entry.getKey()+",Count:"+entry.getValue());
+            //System.out.println("Structure:"+entry.getKey()+"\t"+entry.getValue());
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+    }
+
+    public void PrintTIntIntMap(TIntIntHashMap map, String prefix) {
+        System.out.println(prefix);
+        for (TIntIntIterator it = map.iterator(); it.hasNext(); ) {
+            it.advance();
+            System.out.println("Head:" + it.key() + ",count:" + it.value());
+        }
+    }
 }
