ea93e397831beb656feffb0022eb62354042652b
littlebeanfang <fangwenjing_sjtu@126.com>
2015-02-09 22:04:57 +0800

    no big deal

--- BeanParser/src/Test/DOSHParseTest.java
+++ BeanParser/src/Test/DOSHParseTest.java
@@ -8,7 +8,7 @@ import DOSH.DOSHParse;
 public class DOSHParseTest {
 	public static void main(String args[]) throws ClassNotFoundException, IOException{
 		DOSHParse test=new DOSHParse();
-		String libmodelfile=System.getenv("CODEDATA")+File.separator+"";
+		String libmodelfile=System.getenv("CODEDATA")+File.separator+"wsj_2-21.libmodel";
 		String alphabetfile=System.getenv("CODEDATA")+File.separator+"wsj_2-21.libalphabet";
 		String conllfile=System.getenv("CODEDATA"+File.separator+"1sen.txt");
 		test.TestLibPredict(libmodelfile, alphabetfile, conllfile);
